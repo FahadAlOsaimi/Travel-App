64974c33c4ee0a0f1c9a485ac406da4861a3ee82
FahadMesferS <67862836+FahadMesferS@users.noreply.github.com>
2020-10-06 11:09:51 +0300

    Create geoNames.js

---
+++ src/client/js/geoNames.js
@@ -0,0 +1,16 @@
+async function geo(city, country) {
+  await fetch("http://localhost:5500/geoNames", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ city: city, country: country, message: "PASSED." }),
+  })
+    .then((res) => res.json())
+    .then(function (res) {
+      console.log(res);
+    });
+}
+
+export { geo };
